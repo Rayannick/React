c3c7a55bec4221d626628a133aefde5eb34099a9
Kibrya <efte7123@gmail.com>
2024-09-04 01:13:17 +0600

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,54 @@
-import componentsImg1 from './assets/components.png';
-import { CORE_CONCEPTS  } from './data.js';
-import Header from './component/header.jsx';
-import { CoreConcept , CoreConcept2 } from './component/coreconcept.jsx';
-import TabButton from './component/tabbutton.jsx';
+import componentsImg1 from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./component/header.jsx";
+import { CoreConcept, CoreConcept2 } from "./component/coreconcept.jsx";
+import TabButton from "./component/tabbutton.jsx";
 
-
-
-function App(){
-  function handleSelect(){
-    console.log('A lot of bitches')
+function App() {
+  function handleSelect() {
+    console.log("A lot of bitches");
   }
   return (
     <div>
-<Header></Header>
+      <Header></Header>
       <main>
         <section id="core-concepts">
-        <h2>time to get started</h2>
+          <h2>time to get started</h2>
 
           <ul>
-            <CoreConcept 
-            title = 'components'
-            description = 'the core UI building block'
-            image = {componentsImg1}
-
+            <CoreConcept
+              title="components"
+              description="the core UI building block"
+              image={componentsImg1}
             />
 
+            <CoreConcept2
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
 
-           <CoreConcept2
-           title={CORE_CONCEPTS[1].title}
-           description={CORE_CONCEPTS[1].description}
-           image={CORE_CONCEPTS[1].image}
-           />
+            {/* holy SHIT */}
 
-
-           {/* holy SHIT */}
-
-
-
-           <CoreConcept2 {...CORE_CONCEPTS[2]} />
+            <CoreConcept2 {...CORE_CONCEPTS[2]} />
           </ul>
-
         </section>
 
-        <section id='examples'>
+        {/* name should be same when passing as a function parameter */}
+        <section id="examples">
           <h2>Examples</h2>
           <menu>
-          <TabButton onSelect={handleSelect} >Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>     
-            
-                 </menu>
+            <TabButton bitches={handleSelect}>Components</TabButton>
+            <TabButton bitches={handleSelect}>JSX</TabButton>
+            <TabButton bitches={handleSelect}>Props</TabButton>
+            <TabButton bitches={handleSelect}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
-  )
+  );
 }
 
-console.log('balls bad')
+console.log("balls bad");
 
 export default App;
-
-
-
-
-
-
-
-
-
